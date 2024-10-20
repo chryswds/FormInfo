d1c4b497fed92090eb0eb6dba1b8236a4f0e3789
chryswds <dybas.chrys@gmail.com>
2024-10-20 20:40:07 +0100

    fixing errors

--- express.js
+++ express.js
@@ -2,20 +2,17 @@ var express = require("express");
 var app = express();
 var connection = require("./database");
 
-
-
 connection.connect(function (err) {
   if (err) throw err;
   console.log("Database connected");
 });
-
 
 app.listen(3050, function () {
   console.log("App listening on port 3050");
 });
 
 app.get("/", function (req, res) {
-  let sql = "SELECT * FROM game_db";
+  let sql = "SELECT * FROM player";
   connection.query(sql, function (err, results) {
     if (err) throw err;
     res.send(results);
