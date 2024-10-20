4226a1cc885f93de03360176f961b0b3718b5111
chryswds <dybas.chrys@gmail.com>
2024-10-20 20:30:41 +0100

    correcting errors

--- database.js
+++ database.js
@@ -6,19 +6,3 @@ var connection = mysql.createConnection({
   user: "root",
   password: "password",
 });
-
-application.get("/", function (req, res) {
-  let sql = "SELECT * FROM game_db";
-  connection.query(sql, function (err, results) {
-    if (err) throw err;
-    res.send(results);
-  });
-});
-
-app.listen(3050, function () {
-  console.log("App listening on port 3050");
-  connection.connect(function (err) {
-    if (err) throw err;
-    console.log("Database connected");
-  });
-});
--- express.js
+++ express.js
@@ -1,3 +1,19 @@
 var express = require("express");
 var app = express();
-var connection = require('./database');
+var connection = require("./database");
+
+app.get("/", function (req, res) {
+  let sql = "SELECT * FROM game_db";
+  connection.query(sql, function (err, results) {
+    if (err) throw err;
+    res.send(results);
+  });
+});
+
+app.listen(3050, function () {
+  console.log("App listening on port 3050");
+  connection.connect(function (err) {
+    if (err) throw err;
+    console.log("Database connected");
+  });
+});
