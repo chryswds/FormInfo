b3499328d7fb546ee37a4c71f2e855e210db8858
chryswds <dybas.chrys@gmail.com>
2024-10-20 20:26:46 +0100

    GET Request

--- database.js
+++ database.js
@@ -1,8 +1,16 @@
 var mysql = require("mysql2");
 
 var connection = mysql.createConnection({
-    host: "localhost",
-    database: "game_db",
-    user: "root",
-    password: "password"
+  host: "localhost",
+  database: "game_db",
+  user: "root",
+  password: "password",
+});
+
+application.get("/", function (req, res) {
+  let sql = "SELECT * FROM game_db";
+  connection.query(sql, function (err, results) {
+    if (err) throw err;
+    res.send(results);
+  });
 });
